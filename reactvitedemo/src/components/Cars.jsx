d470b80cfe3fd6f6c38ac3689f356d97166fa779
Thirupathi Addisherla <thirupathiaddisherla@gmail.com>
2025-10-19 22:27:13 +0530

    render array of objects

--- reactvitedemo/src/components/Cars.jsx
+++ reactvitedemo/src/components/Cars.jsx
@@ -1,10 +1,18 @@
 function Cars() {
-  const cars = ["BMW", "Ferrari", "Mercedez", "Audi"];
+  //   const cars = ["BMW", "Ferrari", "Mercedes", "Audi"];
+  const cars = [
+    { name: "BMW", price: "70000" },
+    { name: "Ferrari", price: "80000" },
+    { name: "Mercedes", price: "90000" },
+    { name: "Audi", price: "60000" },
+  ];
   return (
     <>
       <ul>
         {cars.map((car) => (
-          <li key={car}>{car}</li>
+          <li key={car.name}>
+            {car.name} ${car.price}
+          </li>
         ))}
       </ul>
     </>
