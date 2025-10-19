7116b32693877479b6dbd8662c742dbb252d4eef
Thirupathi Addisherla <thirupathiaddisherla@gmail.com>
2025-10-19 22:00:38 +0530

    formatted the code

--- reactvitedemo/src/components/Hello.jsx
+++ reactvitedemo/src/components/Hello.jsx
@@ -1,10 +1,12 @@
-function Hello(props){
-    const { name, greet} = props;
-    return (
-        <div>
-            <h1>hello {name}, {greet}</h1>
-        </div>
-        )
+function Hello(props) {
+  const { name, greet } = props;
+  return (
+    <div>
+      <h1>
+        hello {name}, {greet}
+      </h1>
+    </div>
+  );
 }
 
 export default Hello;
