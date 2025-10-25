e97ca0561bc8ec4d09597d748657a10a9a31c1e4
Thirupathi Addisherla <thirupathiaddisherla@gmail.com>
2025-10-25 22:28:52 +0530

    handling multiple inputs

--- reactvitedemo/src/components/Form.jsx
+++ reactvitedemo/src/components/Form.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 
 function Form() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState({ firstname: "", lastname: "" });
   return (
     <>
       <form>
         <input
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setName({ ...name, firstname: e.target.value })}
           type="text"
-          value={name}
+          value={name.firstname}
+        ></input>
+        <input
+          onChange={(e) => setName({ ...name, lastname: e.target.value })}
+          type="text"
+          value={name.lastname}
         ></input>
       </form>
     </>
