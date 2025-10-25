6e7bb50d2e41f02192abe136255d09c2accee71b
Thirupathi Addisherla <thirupathiaddisherla@gmail.com>
2025-10-25 23:27:55 +0530

    add todos in array on submit

--- todoapp/src/components/Todo.jsx
+++ todoapp/src/components/Todo.jsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 
 function Todo() {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onChange={(e) => setTodo(e.target.value)}
           type="text"
           value={todo}
         ></input>
         <button type="submit">Add</button>
+        {console.log(todos)}
       </form>
     </>
   );
