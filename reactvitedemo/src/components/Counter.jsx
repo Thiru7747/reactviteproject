a048390bad0ddec04ae70dc03313d38d8d22c80f
Thirupathi Addisherla <thirupathiaddisherla@gmail.com>
2025-10-25 21:57:57 +0530

    add increment by

--- reactvitedemo/src/components/Counter.jsx
+++ reactvitedemo/src/components/Counter.jsx
@@ -2,17 +2,27 @@ import { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
-  function handleIncrement(){
-    setCount(count+1);
+  const [incrementBy, setIncrementBy] = useState(1);
+  function handleIncrement() {
+    setCount(count + incrementBy);
   }
-  function handleDecrement(){
-    setCount(count-1);
+  function handleDecrement() {
+    setCount(count - incrementBy);
+  }
+  function handleIncrementBy() {
+    setIncrementBy(incrementBy + 1);
+  }
+  function handleDecrementBy() {
+    setIncrementBy(incrementBy - 1);
   }
   return (
     <>
       <h1>the number is : {count}</h1>
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
+      <h1>increment the count by : {incrementBy}</h1>
+      <button onClick={handleIncrementBy}>Increment By</button>
+      <button onClick={handleDecrementBy}>Decrement By</button>
     </>
   );
 }
